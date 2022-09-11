5902a2e225a7c0040264639f5068f59e49ef9cde
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2022-09-11 23:54:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=ad3d2f57";
+const API_URL = "http://www.omdbapi.com?apikey=942f863f";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
